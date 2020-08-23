d51793b0b65ae663deb5500ddf172dae523beccf
Anna.Tarakanova <anna.tarakanova@nordigy.ru>
2020-08-23 14:36:30 +0300

    Решено 3 задачи 5 модуля

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  let container = document.body.querySelector('.container');
+  container.addEventListener("click", click);
+  let counterRight = 1;
+  let counterLeft = 1;
+  let carouselArrowRight = document.body.querySelector('.carousel__arrow_right');
+  let carouselArrowLeft = document.body.querySelector('.carousel__arrow_left');
+  let carouselInner = document.body.querySelector('.carousel__inner');
+  carouselArrowLeft.style.display = 'none';
+
+  function click(event) {
+    let target = event.target;
+    if (target.closest('.carousel__arrow_right')) {
+      let offset = carouselInner.offsetWidth * counterRight;
+      carouselInner.style.transform = `translateX(-${offset}px)`;
+      counterRight++;
+      counterLeft--;
+      checkArrows(counterLeft, counterRight);
+    }
+    if (target.closest('.carousel__arrow_left')) {
+      let offset = carouselInner.offsetWidth * counterLeft;
+      carouselInner.style.transform = `translateX(${offset}px)`;
+      counterLeft++;
+      counterRight--;
+      checkArrows(counterLeft, counterRight);
+    }
+  }
+
+  function checkArrows(counterLeft, counterRight) {
+    carouselArrowLeft.style.display = (counterLeft === 1 ? 'none' : '');
+    carouselArrowRight.style.display = (counterRight === 4 ? 'none' : '');
+  }
 }
